81ae46cd4df59389a296c25f62afeb7636d89764
Dennis van Bennekom <vanbennekomdennis@gmail.com>
2018-06-14 17:55:24 +0200

    Fixes

---
+++ assets/js/background.js
@@ -0,0 +1,17 @@
+// import randomColor from 'randomcolor';
+//
+// function setColor() {
+//     const color = randomColor({
+//         luminosity: 'light'
+//     });
+//
+//     document.body.style.backgroundColor = color;
+// }
+//
+// setInterval(setColor, 15000);
+// setColor();
+//
+// setTimeout(() => {
+//     document.body.classList.add('active');
+//     setColor();
+// }, 1);
